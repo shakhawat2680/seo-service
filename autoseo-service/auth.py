0dae198a7d0a8818c5701b2c869b903d14ab2189
shakhawat2680 <shakhawat2680@gmail.com>
2026-02-26 23:47:58 +0600

    Update auth.py

--- autoseo-service/auth.py
+++ autoseo-service/auth.py
@@ -1,8 +1,12 @@
 from fastapi import Header, HTTPException
 
-API_KEYS = {"cashog-key": "cashog"}
+API_KEYS = {
+    "cashog-key": "cashog"
+}
+
 
 def verify_api_key(x_api_key: str = Header(...)):
     if x_api_key not in API_KEYS:
         raise HTTPException(status_code=401, detail="Invalid API key")
+
     return API_KEYS[x_api_key]
